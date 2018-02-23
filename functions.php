d9171113cc9267a06e8866a3da39cacd321e1d78
Carrie Forde <carrie@carrieforde.com>
2018-02-22 22:34:55 -0800

    Adds theme constants and cleans up file

--- functions.php
+++ functions.php
@@ -7,82 +7,62 @@
  * @package Aurora_Theme
  */
 
-if ( ! function_exists( 'aurora_theme_setup' ) ) :
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * Note that this function is hooked into the after_setup_theme hook, which
-	 * runs before the init hook. The init hook is too late for some features, such
-	 * as indicating support for post thumbnails.
-	 */
-	function aurora_theme_setup() {
-		/*
-		 * Make theme available for translation.
-		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on Aurora Theme, use a find and replace
-		 * to change 'aurora-theme' to the name of your theme in all the template files.
-		 */
-		load_theme_textdomain( 'aurora-theme', get_template_directory() . '/languages' );
+define( 'AURORA_THEME_VERSION', '1.0.0' );
+define( 'AURORA_THEME_PATH', trailingslashit( get_template_directory() ) );
+define( 'AURORA_THEME_URL', trailingslashit( get_template_directory_uri() ) );
 
-		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
-
-		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
-		add_theme_support( 'title-tag' );
-
-		/*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
-		add_theme_support( 'post-thumbnails' );
-
-		// This theme uses wp_nav_menu() in one location.
-		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'aurora-theme' ),
-		) );
-
-		/*
-		 * Switch default core markup for search form, comment form, and comments
-		 * to output valid HTML5.
-		 */
-		add_theme_support( 'html5', array(
-			'search-form',
-			'comment-form',
-			'comment-list',
-			'gallery',
-			'caption',
-		) );
-
-		// Set up the WordPress core custom background feature.
-		add_theme_support( 'custom-background', apply_filters( 'aurora_theme_custom_background_args', array(
-			'default-color' => 'ffffff',
-			'default-image' => '',
-		) ) );
-
-		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
-
-		/**
-		 * Add support for core custom logo.
-		 *
-		 * @link https://codex.wordpress.org/Theme_Logo
-		 */
-		add_theme_support( 'custom-logo', array(
-			'height'      => 250,
-			'width'       => 250,
-			'flex-width'  => true,
-			'flex-height' => true,
-		) );
-	}
-endif;
 add_action( 'after_setup_theme', 'aurora_theme_setup' );
+/**
+ * Set up theme defaults and registers support for various theme features.
+ */
+function aurora_theme_setup() {
+	
+	// Load the theme translation files.
+	load_theme_textdomain( 'aurora-theme', get_template_directory() . '/languages' );
 
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support( 'automatic-feed-links' );
+
+	// Let WordPress generate the title tags.
+	add_theme_support( 'title-tag' );
+
+	// Enable support for Post Thumbnails on posts and pages.
+	add_theme_support( 'post-thumbnails' );
+
+	// Register a primary and social menu.
+	register_nav_menus( array(
+		'primary' => esc_html__( 'Primary', 'aurora-theme' ),
+		'social' => esc_html__( 'Social', 'aurora-theme' ),
+	) );
+
+	// Use HTML5 where possible.
+	add_theme_support( 'html5', array(
+		'search-form',
+		'comment-form',
+		'comment-list',
+		'gallery',
+		'caption',
+	) );
+
+	// Set up the WordPress core custom background feature.
+	add_theme_support( 'custom-background', apply_filters( 'aurora_theme_custom_background_args', array(
+		'default-color' => 'ffffff',
+		'default-image' => '',
+	) ) );
+
+	// Add theme support for selective refresh for widgets.
+	add_theme_support( 'customize-selective-refresh-widgets' );
+
+	// Add support for core custom logo.
+	add_theme_support( 'custom-logo', array(
+		'height'      => 250,
+		'width'       => 250,
+		'flex-width'  => true,
+		'flex-height' => true,
+	) );
+}
+
+add_action( 'after_setup_theme', 'aurora_theme_content_width', 0 );
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
  *
@@ -93,8 +73,8 @@ add_action( 'after_setup_theme', 'aurora_theme_setup' );
 function aurora_theme_content_width() {
 	$GLOBALS['content_width'] = apply_filters( 'aurora_theme_content_width', 640 );
 }
-add_action( 'after_setup_theme', 'aurora_theme_content_width', 0 );
 
+add_action( 'widgets_init', 'aurora_theme_widgets_init' );
 /**
  * Register widget area.
  *
@@ -111,23 +91,43 @@ function aurora_theme_widgets_init() {
 		'after_title'   => '</h2>',
 	) );
 }
-add_action( 'widgets_init', 'aurora_theme_widgets_init' );
 
+add_action( 'wp_enqueue_scripts', 'aurora_theme_scripts' );
 /**
  * Enqueue scripts and styles.
  */
 function aurora_theme_scripts() {
-	wp_enqueue_style( 'aurora-theme-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'aurora-theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	// Main theme CSS.
+	wp_enqueue_style(
+		'aurora-theme-style',
+		AURORA_THEME_URL . 'style.css',
+		AURORA_THEME_VERSION
+	);
 
-	wp_enqueue_script( 'aurora-theme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	// Theme navagation script.
+	wp_enqueue_script(
+		'aurora-theme-navigation',
+		AURORA_THEME_URL . '/js/navigation.js',
+		array(),
+		AURORA_THEME_VERSION,
+		true
+	);
 
+	// Theme skip link focus fix script.
+	wp_enqueue_script(
+		'aurora-theme-skip-link-focus-fix',
+		AURORA_THEME_URL . '/js/skip-link-focus-fix.js',
+		array(),
+		AURORA_THEME_VERSION,
+		true
+	);
+
+	// Comment reply script.
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
-add_action( 'wp_enqueue_scripts', 'aurora_theme_scripts' );
 
 /**
  * Custom template tags for this theme.
