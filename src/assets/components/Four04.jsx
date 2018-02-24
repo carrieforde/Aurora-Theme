1e70c29ec0b36c290bf3e3ddbdbea5c13634d261
Carrie Forde <carrie@carrieforde.com>
2018-02-24 12:02:29 -0800

    Adds React Router and stubs out a very basic 404

---
+++ src/assets/components/Four04.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Four04 = () => (
+  <section className="404">
+    <header className="page-header">
+      <h1 className="page-title">Oops! We can't find that page!</h1>
+    </header>
+  </section>
+);
+
+export default Four04;
--- src/assets/components/Site.jsx
+++ src/assets/components/Site.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import SkipLink from './SkipLink';
 import Header from './Header';
+import Four04 from './Four04';
 
 const Site = () => (
   <div id="page" className="site">
@@ -9,6 +11,13 @@ const Site = () => (
       title="Aurora Theme"
       description="A clean, modern React-powered theme."
     />
+    <div id="primary" className="content-area">
+      <main id="main" className="site-main" role="main">
+        <BrowserRouter>
+          <Route component={Four04} />
+        </BrowserRouter>
+      </main>
+    </div>
   </div>
 );
 
