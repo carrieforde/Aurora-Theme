d481d840334c3c8eb8eaee6dcb0a53662311dfb0
Carrie Forde <carrie@carrieforde.com>
2018-03-08 19:06:41 -0800

    Adds code for the API request

--- src/assets/scripts/actionCreators.js
+++ src/assets/scripts/actionCreators.js
@@ -1,7 +1,8 @@
+import axios from 'axios';
 import { FETCH_POSTS } from './actions';
 
 /**
- * Returns a well-formatted response.
+ * Returns a well-formatted action object.
  *
  * @export
  * @param {any} posts The posts.
@@ -9,4 +10,19 @@ import { FETCH_POSTS } from './actions';
  */
 export function fetchPosts(posts) {
   return { type: FETCH_POSTS, payload: posts };
+}
+
+// will return a thunk. a function that returns a function.
+export function getAPIData() {
+  return dispatch => {
+    axios
+      .get('http://aurora.local/wp-json/wp/v2/posts')
+      .then(response => {
+        console.log(response.data); // eslint-disable-line no-console
+        dispatch(fetchPosts(response.data));
+      })
+      .catch(error => {
+        console.error('axios error', error);
+      });
+  };
 }
