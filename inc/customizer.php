ab7c5c6c11bc87efe8fbd7ebb3fc806b00debe21
Carrie Forde <carrie@carrieforde.com>
2018-03-23 16:18:35 -0700

    Removes header_textcolor reference

--- inc/customizer.php
+++ inc/customizer.php
@@ -14,7 +14,6 @@ add_action( 'customize_register', 'aurora_theme_customize_register' );
 function aurora_theme_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
-	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
 
 	if ( isset( $wp_customize->selective_refresh ) ) {
 		$wp_customize->selective_refresh->add_partial( 'blogname', array(
