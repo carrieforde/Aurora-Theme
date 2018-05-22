4b923a5127aad63d529f7d0cf28957eca9a66498
Carrie Forde <carrie@carrieforde.com>
2018-05-22 09:14:37 -0700

    Updates text to escape single quote

--- src/Four04/Four04.jsx
+++ src/Four04/Four04.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Four04 = () => (
   <section className="404">
     <header className="page-header">
-      <h1 className="page-title">Oops! We can't find that page!</h1>
+      <h1 className="page-title">Oops! We can&#39;t find that page!</h1>
     </header>
   </section>
 );
